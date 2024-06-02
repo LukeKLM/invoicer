da827153b16955360914e3ed4d096ef3f9d0b72c
Lukáš <lukas.klima@comfortfinancegroup.com>
2024-06-02 13:32:34 +0200

    build(database): change sync to async

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,21 +4,26 @@ from pydantic_core import MultiHostUrl
 
 
 class Settings(BaseSettings):
+    # Auth
+    AUTH_SECRET: str = ""
+
     BACKEND_APP_NAME: str = "My API"
     DEBUG: bool = False
-    POSTGRES_SERVER: str = ""
+
+    # Database
+    POSTGRES_SERVER: str = "localhost"
     POSTGRES_PORT: int = 5432
     POSTGRES_USER: str = ""
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
 
     class Config:
-        env_file = "../.env"
+        env_file = ".env"
 
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:  # noqa N802 should be lowercase
         return MultiHostUrl.build(
-            scheme="postgresql+psycopg",
+            scheme="postgresql+asyncpg",
             username=self.POSTGRES_USER,
             password=self.POSTGRES_PASSWORD,
             host=self.POSTGRES_SERVER,
--- backend/core/db.py
+++ backend/core/db.py
@@ -1,18 +1,19 @@
-from collections.abc import Generator
+from collections.abc import AsyncGenerator
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+print(settings.SQLALCHEMY_DATABASE_URI)
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db() -> Generator[Session, None]:
-    with SessionLocal() as session:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
         yield session
 
 
