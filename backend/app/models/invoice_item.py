2580f0ee04d4727601e01db120a71417d693d9e6
Lukáš Klíma <Lukas.klm94@gmail.com>
2024-11-03 09:04:38 +0100

    feat: invoice item model

---
+++ backend/app/models/invoice_item.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import Numeric
+from sqlalchemy import String
+
+from core.db import BaseModel
+
+
+class InvoiceItem(BaseModel):
+    __tablename__ = "invoice_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    invoice_id = Column(Integer, nullable=False)
+    price = Column(Numeric(precision=10, scale=2), nullable=False)
+    title = Column(String(64), nullable=False)
+    quantity = Column(Integer, nullable=False, default=1)
