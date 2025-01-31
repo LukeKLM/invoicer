333b4af19a251c5675e31c5081d05e7cf17d3755
Lukáš Klíma <Lukas.klm94@gmail.com>
2025-01-31 14:43:04 +0100

    feat: customers dic_id nullable

--- backend/app/schemas/customers.py
+++ backend/app/schemas/customers.py
@@ -6,7 +6,7 @@ class CreateInvoiceCustomer(BaseModel):
     email: str = Field(max_length=255)
     name: str = Field(max_length=255)
     vat_id: str = Field(max_length=50)
-    dic_id: str = Field(max_length=50)
+    dic_id: str | None = Field(max_length=50)
 
     city: str = Field(max_length=255)
     country: str = Field(max_length=255)
